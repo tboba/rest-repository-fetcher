ee31e1798d08e381d27bee5aa8e060dda05c212b
Tymoteusz Boba <Tymoteusz.Boba@gmail.com>
2021-04-21 02:32:46 +0200

    Create initial commit, add base configuration files.

---
+++ src/main/java/pl/tboba/repositoryfetcher/RepositoryFetcherApplication.java
@@ -0,0 +1,13 @@
+package pl.tboba.repositoryfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryFetcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/tboba/repositoryfetcher/RepositoryFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tboba.repositoryfetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
